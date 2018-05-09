cd3cd12e3ec9a65bc943bd3d32d512b512f934fe
william <willshinji@gmail.com>
2018-05-09 15:10:46 -0300

    initial commit

---
+++ array_methods.js
@@ -0,0 +1,19 @@
+//map, filter, find, every, some, reduce, etc
+
+let array = [1, 2, 3, 4, 5];
+
+array = array
+            .map(item => item * 2)
+            .filter(item => item > 5)
+console.log(array);
+
+
+//instead of this
+// var array = [1, 2, 3, 4, 5];
+// var items = [];
+// for(var i = 0; i < array.length; i++){
+//   if ((array[i] * 2) > 5){
+//     items.push(array[i] * 2);
+//   }
+// }
+// console.log(items);
